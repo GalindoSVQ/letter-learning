c27d6c10c5a24631230f312338c2663f4bdeeed2
Antonio Galindo <galindosvq@gmail.com>
2022-01-16 21:25:16 +0100

    chore: add images

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 const root = document.getElementById("root");
+
+const alphabet = [
+  { name: "A" },
+  { name: "B" },
+  { name: "C" },
+  { name: "D" },
+  { name: "E" },
+  { name: "F" },
+  { name: "G" },
+  { name: "H" },
+  { name: "I" },
+  { name: "J" },
+  { name: "K" },
+  { name: "L" },
+  { name: "M" },
+  { name: "N" },
+  { name: "Ñ" },
+  { name: "O" },
+  { name: "P" },
+  { name: "Q" },
+  { name: "R" },
+  { name: "S" },
+  { name: "T" },
+  { name: "U" },
+  { name: "V" },
+  { name: "W" },
+  { name: "X" },
+  { name: "Y" },
+  { name: "Z" },
+];
+
+const letterCard = (letter) => {
+  return (
+    <div>
+      <p>{p.name}</p>
+    </div>
+  );
+};
+
+root.innerHTML = alphabet.map((letter) => letter);
